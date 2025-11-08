886568127060f4d08ca85c5fd801e77aa11239c8
ved <vedpatel071203@gmail.com>
2025-11-08 13:18:13 +0530

    Secured credentials using .env and added dotenv configuration

--- src/main/java/com/project/EzeeRideApplication.java
+++ src/main/java/com/project/EzeeRideApplication.java
@@ -1,5 +1,6 @@
 package com.project;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,15 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 @EnableScheduling
 public class EzeeRideApplication {
+
+    static {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("SMTP_USERNAME", dotenv.get("SMTP_USERNAME"));
+        System.setProperty("SMTP_PASSWORD", dotenv.get("SMTP_PASSWORD"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EzeeRideApplication.class, args);
