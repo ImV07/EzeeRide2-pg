ad5e61800ab27d0784b89c0eb73d45fe4ba64b9a
ved <vedpatel071203@gmail.com>
2025-12-08 19:05:01 +0530

    added

---
+++ src/main/java/com/project/dto/ServiceRequestDTO.java
@@ -0,0 +1,50 @@
+package com.project.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.model.Vehicle;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+public class ServiceRequestDTO {
+
+    private String serviceDescription;
+
+    private LocalDate serviceDate;
+
+    private double servicingCost;
+
+    private String remarks;
+
+    public String getServiceDescription() {
+        return serviceDescription;
+    }
+
+    public void setServiceDescription(String serviceDescription) {
+        this.serviceDescription = serviceDescription;
+    }
+
+    public LocalDate getServiceDate() {
+        return serviceDate;
+    }
+
+    public void setServiceDate(LocalDate serviceDate) {
+        this.serviceDate = serviceDate;
+    }
+
+    public double getServicingCost() {
+        return servicingCost;
+    }
+
+    public void setServicingCost(double servicingCost) {
+        this.servicingCost = servicingCost;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
